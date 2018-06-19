10ee902e0f86d7f6b8e34fe0b3f314709630a572
Rachel Westwater <rachel-westwater@hotmail.com>
2018-06-19 16:50:46 +0100

    Park class created

---
+++ models/park.js
@@ -0,0 +1,7 @@
+const Park = function (name, ticketPrice, collectionOfDinosaurs) {
+  this.name = name;
+  this.ticketPrice = ticketPrice;
+  this.collectionOfDinosaurs = [];
+}
+
+module.exports = Park;
