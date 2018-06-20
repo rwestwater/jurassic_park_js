4eedebf2118e3fdb2e7e563f66a399118971a7f4
Rachel Westwater <rachel-westwater@hotmail.com>
2018-06-20 09:04:04 +0100

    final commit

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -36,7 +36,6 @@ describe('Park', function() {
     assert.deepStrictEqual(park.collectionOfDinosaurs, [dinosaur]);
   });
 
-
   it('should be able to find the dinosaur that attracts the most visitors');
 
   it('should be able to find all dinosaurs of a particular species');
